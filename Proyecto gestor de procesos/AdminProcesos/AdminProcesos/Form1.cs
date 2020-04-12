0589649b1c151c5414c8b82205917eb0de5995d0
Dalex93 <54257110+Dalex93@users.noreply.github.com>
2020-04-12 02:56:47 -0600

    Terminado el foreach

--- Proyecto gestor de procesos/AdminProcesos/AdminProcesos/Form1.cs
+++ Proyecto gestor de procesos/AdminProcesos/AdminProcesos/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace AdminProcesos
 {
@@ -15,6 +16,31 @@ namespace AdminProcesos
         public Form1()
         {
             InitializeComponent();
+            ActualizarProcesos();
+        }
+
+        //Funcion que va a limpiar y actualizar los procesos
+        private void ActualizarProcesos()
+        {
+            lstprocesos.Items.Clear();
+            lstid.Items.Clear();
+            lstram.Items.Clear();
+            lstvirtual.Items.Clear();
+            lstcpu.Items.Clear();
+            
+            int id = 1;
+            foreach (Process procesos in Process.GetProcesses())
+            {
+                lstprocesos.Items.Add(id + ":" + procesos.ProcessName);
+                lstid.Items.Add(id + ":" + procesos.Id);
+                lstram.Items.Add(id + ":" + procesos.WorkingSet64);
+                lstvirtual.Items.Add(id + ":" + procesos.VirtualMemorySize64);
+                lstcpu.Items.Add(id + ":" + procesos.SessionId);
+                id = id + 1;
+            }
+            lbltotalprocesos.Text = "Total de procesos : " + lstprocesos.Items.Count.ToString();
+
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
