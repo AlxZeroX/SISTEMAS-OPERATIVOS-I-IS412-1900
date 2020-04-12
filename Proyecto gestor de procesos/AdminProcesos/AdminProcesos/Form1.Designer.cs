1e899f708cdf3147cd97c123336aae9db143ef32
Dalex93 <54257110+Dalex93@users.noreply.github.com>
2020-04-12 00:54:25 -0600

    Creacion de los botones

--- Proyecto gestor de procesos/AdminProcesos/AdminProcesos/Form1.Designer.cs
+++ Proyecto gestor de procesos/AdminProcesos/AdminProcesos/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.listBox3 = new System.Windows.Forms.ListBox();
             this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -45,7 +49,7 @@
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(235, 61);
+            this.listBox2.Location = new System.Drawing.Point(226, 61);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(151, 277);
             this.listBox2.TabIndex = 1;
@@ -53,7 +57,7 @@
             // listBox3
             // 
             this.listBox3.FormattingEnabled = true;
-            this.listBox3.Location = new System.Drawing.Point(457, 61);
+            this.listBox3.Location = new System.Drawing.Point(427, 61);
             this.listBox3.Name = "listBox3";
             this.listBox3.Size = new System.Drawing.Size(151, 277);
             this.listBox3.TabIndex = 2;
@@ -61,16 +65,55 @@
             // listBox4
             // 
             this.listBox4.FormattingEnabled = true;
-            this.listBox4.Location = new System.Drawing.Point(725, 61);
+            this.listBox4.Location = new System.Drawing.Point(622, 61);
             this.listBox4.Name = "listBox4";
             this.listBox4.Size = new System.Drawing.Size(151, 277);
             this.listBox4.TabIndex = 3;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(836, 61);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(151, 277);
+            this.listBox5.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 35);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(276, 398);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 37);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(452, 401);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 34);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1034, 509);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox5);
             this.Controls.Add(this.listBox4);
             this.Controls.Add(this.listBox3);
             this.Controls.Add(this.listBox2);
@@ -88,6 +131,10 @@
         private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.ListBox listBox3;
         private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
