0a9cc71f3db919c83a4b84406f3804b8094bc039
Dalex93 <54257110+Dalex93@users.noreply.github.com>
2020-04-12 01:29:33 -0600

    Finalizacion parte grafica

--- Proyecto gestor de procesos/AdminProcesos/AdminProcesos/Form1.Designer.cs
+++ Proyecto gestor de procesos/AdminProcesos/AdminProcesos/Form1.Designer.cs
@@ -28,152 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.listBox2 = new System.Windows.Forms.ListBox();
-            this.listBox3 = new System.Windows.Forms.ListBox();
-            this.listBox4 = new System.Windows.Forms.ListBox();
-            this.listBox5 = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.lstprocesos = new System.Windows.Forms.ListBox();
+            this.lstid = new System.Windows.Forms.ListBox();
+            this.lstram = new System.Windows.Forms.ListBox();
+            this.lstvirtual = new System.Windows.Forms.ListBox();
+            this.lstcpu = new System.Windows.Forms.ListBox();
+            this.btnactualizar = new System.Windows.Forms.Button();
+            this.btncerrar = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.lbltotalprocesos = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // listBox1
+            // lstprocesos
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(26, 75);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(151, 277);
-            this.listBox1.TabIndex = 0;
+            this.lstprocesos.FormattingEnabled = true;
+            this.lstprocesos.Location = new System.Drawing.Point(26, 75);
+            this.lstprocesos.Name = "lstprocesos";
+            this.lstprocesos.Size = new System.Drawing.Size(151, 277);
+            this.lstprocesos.TabIndex = 0;
             // 
-            // listBox2
+            // lstid
             // 
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(225, 75);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(151, 277);
-            this.listBox2.TabIndex = 1;
+            this.lstid.FormattingEnabled = true;
+            this.lstid.Location = new System.Drawing.Point(225, 75);
+            this.lstid.Name = "lstid";
+            this.lstid.Size = new System.Drawing.Size(151, 277);
+            this.lstid.TabIndex = 1;
             // 
-            // listBox3
+            // lstram
             // 
-            this.listBox3.FormattingEnabled = true;
-            this.listBox3.Location = new System.Drawing.Point(425, 75);
-            this.listBox3.Name = "listBox3";
-            this.listBox3.Size = new System.Drawing.Size(151, 277);
-            this.listBox3.TabIndex = 2;
+            this.lstram.FormattingEnabled = true;
+            this.lstram.Location = new System.Drawing.Point(425, 75);
+            this.lstram.Name = "lstram";
+            this.lstram.Size = new System.Drawing.Size(151, 277);
+            this.lstram.TabIndex = 2;
             // 
-            // listBox4
+            // lstvirtual
             // 
-            this.listBox4.FormattingEnabled = true;
-            this.listBox4.Location = new System.Drawing.Point(621, 75);
-            this.listBox4.Name = "listBox4";
-            this.listBox4.Size = new System.Drawing.Size(151, 277);
-            this.listBox4.TabIndex = 3;
+            this.lstvirtual.FormattingEnabled = true;
+            this.lstvirtual.Location = new System.Drawing.Point(621, 75);
+            this.lstvirtual.Name = "lstvirtual";
+            this.lstvirtual.Size = new System.Drawing.Size(151, 277);
+            this.lstvirtual.TabIndex = 3;
             // 
-            // listBox5
+            // lstcpu
             // 
-            this.listBox5.FormattingEnabled = true;
-            this.listBox5.Location = new System.Drawing.Point(836, 75);
-            this.listBox5.Name = "listBox5";
-            this.listBox5.Size = new System.Drawing.Size(151, 277);
-            this.listBox5.TabIndex = 4;
+            this.lstcpu.FormattingEnabled = true;
+            this.lstcpu.Location = new System.Drawing.Point(836, 75);
+            this.lstcpu.Name = "lstcpu";
+            this.lstcpu.Size = new System.Drawing.Size(151, 277);
+            this.lstcpu.TabIndex = 4;
             // 
-            // button1
+            // btnactualizar
             // 
-            this.button1.Location = new System.Drawing.Point(26, 401);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 35);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnactualizar.Location = new System.Drawing.Point(43, 389);
+            this.btnactualizar.Name = "btnactualizar";
+            this.btnactualizar.Size = new System.Drawing.Size(101, 38);
+            this.btnactualizar.TabIndex = 5;
+            this.btnactualizar.Text = "Actualizar";
+            this.btnactualizar.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btncerrar
             // 
-            this.button2.Location = new System.Drawing.Point(453, 401);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(101, 37);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btncerrar.Location = new System.Drawing.Point(451, 390);
+            this.btncerrar.Name = "btncerrar";
+            this.btncerrar.Size = new System.Drawing.Size(101, 37);
+            this.btncerrar.TabIndex = 6;
+            this.btncerrar.Text = "Cerrar";
+            this.btncerrar.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnsalir
             // 
-            this.button3.Location = new System.Drawing.Point(886, 401);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(101, 34);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnsalir.Location = new System.Drawing.Point(856, 391);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(101, 34);
+            this.btnsalir.TabIndex = 7;
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.button3_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 31);
+            this.label1.Location = new System.Drawing.Point(76, 44);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(51, 13);
             this.label1.TabIndex = 8;
-            this.label1.Text = "label1";
+            this.label1.Text = "Procesos";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(222, 31);
+            this.label2.Location = new System.Drawing.Point(267, 44);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(65, 13);
             this.label2.TabIndex = 9;
-            this.label2.Text = "label2";
+            this.label2.Text = "Identificador";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(422, 31);
+            this.label3.Location = new System.Drawing.Point(459, 44);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(74, 13);
             this.label3.TabIndex = 10;
-            this.label3.Text = "label3";
+            this.label3.Text = "Memoria RAM";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(628, 26);
+            this.label4.Location = new System.Drawing.Point(660, 44);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(78, 13);
             this.label4.TabIndex = 11;
-            this.label4.Text = "label4";
+            this.label4.Text = "Memoria virtual";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(842, 27);
+            this.label5.Location = new System.Drawing.Point(869, 44);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(88, 13);
             this.label5.TabIndex = 12;
-            this.label5.Text = "label5";
+            this.label5.Text = "Procesador/CPU";
+            // 
+            // lbltotalprocesos
+            // 
+            this.lbltotalprocesos.AutoSize = true;
+            this.lbltotalprocesos.Location = new System.Drawing.Point(37, 534);
+            this.lbltotalprocesos.Name = "lbltotalprocesos";
+            this.lbltotalprocesos.Size = new System.Drawing.Size(35, 13);
+            this.lbltotalprocesos.TabIndex = 13;
+            this.lbltotalprocesos.Text = "label6";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1064, 596);
+            this.Controls.Add(this.lbltotalprocesos);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.listBox5);
-            this.Controls.Add(this.listBox4);
-            this.Controls.Add(this.listBox3);
-            this.Controls.Add(this.listBox2);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btncerrar);
+            this.Controls.Add(this.btnactualizar);
+            this.Controls.Add(this.lstcpu);
+            this.Controls.Add(this.lstvirtual);
+            this.Controls.Add(this.lstram);
+            this.Controls.Add(this.lstid);
+            this.Controls.Add(this.lstprocesos);
             this.Name = "Form1";
             this.Text = "Gestor de procesos";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -184,19 +197,20 @@
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.ListBox listBox2;
-        private System.Windows.Forms.ListBox listBox3;
-        private System.Windows.Forms.ListBox listBox4;
-        private System.Windows.Forms.ListBox listBox5;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox lstprocesos;
+        private System.Windows.Forms.ListBox lstid;
+        private System.Windows.Forms.ListBox lstram;
+        private System.Windows.Forms.ListBox lstvirtual;
+        private System.Windows.Forms.ListBox lstcpu;
+        private System.Windows.Forms.Button btnactualizar;
+        private System.Windows.Forms.Button btncerrar;
+        private System.Windows.Forms.Button btnsalir;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbltotalprocesos;
     }
 }
 
--- Proyecto gestor de procesos/AdminProcesos/AdminProcesos/Form1.cs
+++ Proyecto gestor de procesos/AdminProcesos/AdminProcesos/Form1.cs
@@ -26,5 +26,15 @@ namespace AdminProcesos
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
