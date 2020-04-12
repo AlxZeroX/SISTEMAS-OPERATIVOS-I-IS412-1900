6f8a4fe77f0defccbda961ad8d8db9b457f81516
Dalex93 <54257110+Dalex93@users.noreply.github.com>
2020-04-12 00:57:50 -0600

    Botones y etiquetas

--- Proyecto gestor de procesos/AdminProcesos/AdminProcesos/Form1.Designer.cs
+++ Proyecto gestor de procesos/AdminProcesos/AdminProcesos/Form1.Designer.cs
@@ -36,12 +36,17 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(26, 61);
+            this.listBox1.Location = new System.Drawing.Point(26, 75);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(151, 277);
             this.listBox1.TabIndex = 0;
@@ -49,7 +54,7 @@
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(226, 61);
+            this.listBox2.Location = new System.Drawing.Point(225, 75);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(151, 277);
             this.listBox2.TabIndex = 1;
@@ -57,7 +62,7 @@
             // listBox3
             // 
             this.listBox3.FormattingEnabled = true;
-            this.listBox3.Location = new System.Drawing.Point(427, 61);
+            this.listBox3.Location = new System.Drawing.Point(425, 75);
             this.listBox3.Name = "listBox3";
             this.listBox3.Size = new System.Drawing.Size(151, 277);
             this.listBox3.TabIndex = 2;
@@ -65,7 +70,7 @@
             // listBox4
             // 
             this.listBox4.FormattingEnabled = true;
-            this.listBox4.Location = new System.Drawing.Point(622, 61);
+            this.listBox4.Location = new System.Drawing.Point(621, 75);
             this.listBox4.Name = "listBox4";
             this.listBox4.Size = new System.Drawing.Size(151, 277);
             this.listBox4.TabIndex = 3;
@@ -73,14 +78,14 @@
             // listBox5
             // 
             this.listBox5.FormattingEnabled = true;
-            this.listBox5.Location = new System.Drawing.Point(836, 61);
+            this.listBox5.Location = new System.Drawing.Point(836, 75);
             this.listBox5.Name = "listBox5";
             this.listBox5.Size = new System.Drawing.Size(151, 277);
             this.listBox5.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(106, 400);
+            this.button1.Location = new System.Drawing.Point(26, 401);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(101, 35);
             this.button1.TabIndex = 5;
@@ -89,7 +94,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(276, 398);
+            this.button2.Location = new System.Drawing.Point(453, 401);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(101, 37);
             this.button2.TabIndex = 6;
@@ -98,18 +103,69 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(452, 401);
+            this.button3.Location = new System.Drawing.Point(886, 401);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(101, 34);
             this.button3.TabIndex = 7;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(422, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(628, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(842, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "label5";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1034, 509);
+            this.ClientSize = new System.Drawing.Size(1064, 596);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -122,6 +178,7 @@
             this.Text = "Gestor de procesos";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -135,6 +192,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
